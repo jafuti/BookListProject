4572c90066d86579b211adc6dc3b9d50476ab03b
jafar <jafare2010@gmail.com>
2019-12-14 15:39:06 +0300

    asp.net core project

---
+++ BooKList/Controllers/BookController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BooKList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooKList.Controllers
+{
+    public class BookController : Controller
+    {
+
+        //needs to have the database object to retrieve or update from the database
+        private readonly BookDbContext _db;
+
+        //intiallizing the _db 
+        public BookController(BookDbContext db)
+        {
+            //using _db to access the database
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //Returning all the books from the database
+            return View(_db.Books.ToList());
+        }
+
+        //Get: Book/Create
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        //calling the post action method to add the book to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+        //Details : Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+        //Edit : Books/Details/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //POST: Book/Edit/3
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            if(id != book.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+
+
+        //Delete : Books/Details/3
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //Delete : Book/Delete/3
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveBook(int id)
+        {
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //to dispose _db after it usage
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            };
+        }
+    }
+}
